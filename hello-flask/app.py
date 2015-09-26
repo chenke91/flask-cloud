9b8bef94811967efd5a555e67cedb427014e64d7
chenke91 <chenke91@qq.com>
2015-09-27 01:24:55 +0800

    add host name

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return 'hello world from {}!'.format(socket.gethostname())
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
